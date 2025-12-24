1543a6800b7752caa60d4899e814cdfffe62b8b7
dinana <dinasholilkhah@gmail.com>
2025-12-24 23:17:08 +0700

    Update sorotan.py

--- app/model/sorotan.py
+++ app/model/sorotan.py
@@ -2,20 +2,47 @@ from app import db
 from datetime import datetime
 
 class Sorotan(db.Model):
-    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    __tablename__ = 'sorotan'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nama_sorotan = db.Column(db.String(250), nullable=False)
     gambar_sorotan = db.Column(db.String(500), nullable=False)
     waktu_sorotan = db.Column(db.Integer, nullable=False)
-    kategori_id = db.Column(db.BigInteger, db.ForeignKey('kategori.id'), nullable=False)
+
+    kategori_id = db.Column(
+        db.Integer,
+        db.ForeignKey('kategori.id'),
+        nullable=False
+    )
+
     deskripsi_sorotan = db.Column(db.Text, nullable=False)
     alat_sorotan = db.Column(db.Text, nullable=False)
     langkah_sorotan = db.Column(db.Text, nullable=False)
-    penulis = db.Column(db.BigInteger, db.ForeignKey('user.id'), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    kategori = db.relationship('Kategori', backref=db.backref('sorotan', lazy=True))
-    pembuat = db.relationship('User', backref=db.backref('sorotan', lazy=True))
+    penulis = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id'),
+        nullable=False
+    )
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
+
+    # Relationships
+    kategori = db.relationship(
+        'Kategori',
+        backref=db.backref('sorotan_list', lazy=True)
+    )
+
+    pembuat = db.relationship(
+        'User',
+        foreign_keys=[penulis],
+        backref=db.backref('sorotan_list', lazy=True)
+    )
 
     def __repr__(self):
-        return "<Sorotan {} - {}>".format(self.id, self.nama_sorotan)
+        return f"<Sorotan {self.id} - {self.nama_sorotan}>"
