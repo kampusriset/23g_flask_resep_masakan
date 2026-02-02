11cd26ecfa40026eb14bdf4efc2fc98d9a58d0f8
ngestiutamii <ngestiutami67@gmail.com>
2026-02-01 22:14:42 -0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -12,12 +12,25 @@ if os.getenv('DB_DRIVER', 'sqlite').lower() == 'mysql':
 else:
     from config import Config as DBConfig
 
-app = Flask(__name__)
-app.config.from_object(DBConfig)
-app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
+def strip_numbering(text):
+    import re
+    return re.sub(r'^[0-9]+\.?\s*', '', text.strip())
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.jinja_env.filters['strip_numbering'] = strip_numbering
+    app.config.from_object(DBConfig)
+    app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
+    return app
+
+app = create_app()
 
 from app.model import user, kategori, resep, sorotan, favorit
 from app import routes
